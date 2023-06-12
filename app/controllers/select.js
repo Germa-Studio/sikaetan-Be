d08b23d41b143aedfd499114a39af86ce8f386fb
heri2610 <suhaeriheri45@gmail.com>
2023-06-13 04:10:21 +0700

    menambahkan lagi

--- app/controllers/select.js
+++ app/controllers/select.js
@@ -4,7 +4,8 @@ const selectTani = async(req, res)=>{
   try {
     const {desa} =req.body
     const data = await kelompok.findAll({where:{desa}});
-    const penyuluh = await dataPenyuluh.findAll({where:{desa}, include:[{model:dataPerson}]});
+    // const penyuluh = await dataPenyuluh.findAll({where:{desa}, include:[{model:dataPerson}]});
+    const penyuluh = await dataPerson.findAll({where:{desa, role:"penyuluh"}});
     res.status(200).json({
       message: 'Berhasil Mendapatkan Data Info Tani',
       kelompok:data,
