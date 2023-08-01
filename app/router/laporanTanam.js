5c7e38b99c9299373f0c80bbe635ac4b29a1c16c
heri2610 <suhaeriheri45@gmail.com>
2023-08-01 13:03:58 +0700

    menambahakan

--- app/router/laporanTanam.js
+++ app/router/laporanTanam.js
@@ -15,6 +15,6 @@ router.put('/laporan-tanam/:id', auth,upload.single('fotoTanaman'), editLaporanT
 router.delete('/laporan-tanam/:id', auth,   deleteLaporanTanam);
 router.get('/laporan-tanam/:id', auth, getAllLaporanTanam);
 router.get('/laporan-tanam/detail/:id', auth, getLaporanTanamById);
-router.post('/laporan-akhir', auth, tambahLaporanAkhir);
+router.post('/laporan-akhir', auth, upload.single('fotoTanaman'), tambahLaporanAkhir);
 
 module.exports = router;
